0e29103d91c54439cdebad47a8b620ad35d0e8f6
wuhao <wuhaosky@163.com>
2016-04-24 15:01:58 +0800

    我是张三

---
+++ OSChina/OSChina/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  OSChina
+//
+//  Created by 吴昊 on 16/4/24.
+//  Copyright © 2016年 wuhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ OSChina/OSChina/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  OSChina
+//
+//  Created by 吴昊 on 16/4/24.
+//  Copyright © 2016年 wuhao. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
